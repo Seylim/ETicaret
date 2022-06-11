bda7c44adf684035f5aab911f58053d6c483e535
DESKTOP-VADD38C\emrek <seylimemre@gmail.com>
2022-06-11 17:09:32 +0300

    DTOs

---
+++ Dtos/Requests/AddProductRequest.cs
@@ -0,0 +1,23 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dtos.Requests
+{
+    public class AddProductRequest
+    {
+        public string Name { get; set; }
+        public string TradeMark { get; set; }
+        public short Stock { get; set; }
+        public string Description { get; set; }
+        public int Discount { get; set; }
+        public float PurchasePrice { get; set; }
+        public float SalePrice { get; set; }
+        public string ImageUrl { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
---
+++ Dtos/Requests/UpdateProductRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dtos.Requests
+{
+    public class UpdateProductRequest
+    {
+        public int Id { get; set; }
+        public short Stock { get; set; }
+        public string Description { get; set; }
+        public int Discount { get; set; }
+        public float PurchasePrice { get; set; }
+        public float SalePrice { get; set; }
+        public string ImageUrl { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
